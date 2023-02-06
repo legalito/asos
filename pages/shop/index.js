f622d95ad372cd073aea970545b3d55789abac66
noa <nxa.drxuineau@gmail.com>
2023-02-06 15:14:52 +0100

    add card to shop

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 import Card from '@/component/card/card'
 import styles from './shop.module.css'
+import Navbar from '@/component/navbar/navbar'
+import Footer from '@/component/footer/footer'
 
 export class index extends Component {
   render() {
     return (
-      <div className={styles.card}>
-        <Card/>
-        
+      
+      <div className={styles.content}>
+        <Navbar/>
+        <div className={styles.shop}>
+        <div className={styles.filterMenu}>
+
+        </div>
+        <div className={styles.cardDisplay}>
+          <div className={styles.card}>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+          </div> 
+        </div>
+        </div>
+        <Footer/>
       </div>
     )
   }
