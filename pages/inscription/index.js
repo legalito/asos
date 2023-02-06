ddadad427156867bf75176cdb7e57c4c03c96710
tidjani98 <90622309+tidjani98@users.noreply.github.com>
2023-02-06 16:10:04 +0100

    inscription

--- pages/inscription/index.js
+++ pages/inscription/index.js
@@ -2,49 +2,39 @@ import React, { Component } from 'react'
 import styles from './inscription.module.css'
 import Navbar from '@/component/navbar/navbar'
 import footer from '@/component/footer/footer'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
+
+
 
 export class index extends Component {
-  return (
-    <>
-    <div className={styles.container}>
-    <div className={styles.sing}
-    <div>
-      <h1>inscription</h1>
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        validate={values => {
-          const errors = {}
-          if (!values.email) {
-            errors.email = 'Required'
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address'
-          }
-          return errors
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2))
-            setSubmitting(false)
-          }, 400)
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" />
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  )
+  render() {
+    return (
+      <>
+      <div className={styles.containerLog}>
+      <div className={styles.singupWrapper}>
+        <h1>inscription</h1>
+        <form className={styles.form}>
+            <label>
+            <p>email</p>
+            <input type="email" />
+            </label>
+            <label>
+            <p>Username</p>
+            <input type="text" />
+            </label>
+            <label>
+            <p>Password</p>
+            <input type="password" />
+            </label>
+            <div>
+            <button type="login">Login</button>
+            </div>
+        </form>
+        </div>
+      </div>
+        
+      </>
+    )
+  }
 }
 
 export default index
