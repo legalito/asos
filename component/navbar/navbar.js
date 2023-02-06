3f1819a921048860532c04cf435730da8bdbfc0a
folecrom <90184290+folecrom@users.noreply.github.com>
2023-02-06 14:27:40 +0100

    yes
    test

--- component/navbar/navbar.js
+++ component/navbar/navbar.js
@@ -13,14 +13,8 @@ export class navbar extends Component {
                 <div className={styles.navbar}>
                     <div>
                         <p>Accueil</p>
-<<<<<<< HEAD:component/navbar.js
-                        <p>Accueil</p>
-                        <p>Accueil</p>
-                        <p>panier</p>
-=======
                         <p>Shop</p>
                         <p>Accueil</p>
->>>>>>> main:component/navbar/navbar.js
                         <div>
                             <Link className={styles.Link} href="/inscription"><button>Inscription</button></Link>
                             <Link className={styles.Link} href="/connexion"><button>Connexion</button></Link>
