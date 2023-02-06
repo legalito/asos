3629158204f3f7e255ad8a6be79c9ed0519a5034
legalito <63124952+legalito@users.noreply.github.com>
2023-02-06 14:54:09 +0100

    up

--- pages/connexion/index.js
+++ pages/connexion/index.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react'
 import styles from './connexion.module.css'
 import Navbar from '@/component/navbar/navbar'
-import footer from '@/component/footer/footer'
+import Footer from '@/component/footer/footer'
 export class index extends Component {
   render() {
     return (
       <>
       <div className={styles.containerLog}>
+        <Navbar/>
       <div className={styles.loginWrapper}>
         <h1>Please Log In</h1>
         <form className={styles.form}>
-            <label>
-            <p>Username</p>
+            <label className={styles.label}><p>Username</p></label>
             <input type="text" />
-            </label>
-            <label>
-            <p>Password</p>
+            <label className={styles.label}><p>Password</p></label>
             <input type="password" />
-            </label>
             <div>
-            <button type="submit">Submit</button>
+              <button type="submit">Submit</button>
             </div>
         </form>
         </div>
+        <Footer/>
       </div>
         
       </>
