c1cdaa3ef18c5f2b97baaa904a5e6d4522aec439
folecrom <90184290+folecrom@users.noreply.github.com>
2023-02-07 02:47:43 +0100

    panier

--- pages/panier/index.js
+++ pages/panier/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from '@/component/navbar/navbar'
 import styles from './panier.module.css'
+import Footer from '@/component/footer/footer'
 
 export class panier extends Component {
   render() {
@@ -8,11 +9,32 @@ export class panier extends Component {
       <div>
         <Navbar />
         <div>
-          <div className={styles.title}><h1>panier</h1></div>
+          <div className={styles.title}><h1>Panier</h1></div>
           <div className={styles.trait_dessus}>
-          <p>article</p></div>
+          <p>article</p>
+          <div className={styles.quantite}>
+          <select>
+            <option>quantité</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+          </select></div></div>
+          <div className={styles.box}>
+            <p>Commande :</p>
+            <div className={styles.box2}>
+              <p>quantité produit</p>
+              <div className={styles.trait_dessous2}></div>
+              <p>Total produit</p>
+              <div className={styles.trait_dessous2}></div>
+              <p>prix livraison :</p>
+              <div className={styles.trait_dessous2}></div>
+              <p>total commande : 687 $</p>
+              </div>
+          </div>
           <div className={styles.trait_dessous}></div>
-        </div>
+        </div>  
+          <div className={styles.Footer}><Footer/></div>
       </div>
     )
   }
