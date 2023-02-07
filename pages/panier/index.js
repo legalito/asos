81fbda12b29233a48894ce56872bfc6e8de59b94
folecrom <90184290+folecrom@users.noreply.github.com>
2023-02-07 02:53:09 +0100

    panier

--- pages/panier/index.js
+++ pages/panier/index.js
@@ -11,17 +11,17 @@ export class panier extends Component {
         <div>
           <div className={styles.title}><h1>Panier</h1></div>
           <div className={styles.trait_dessus}>
-          <p>article</p>
-          <div className={styles.quantite}>
-          <select>
-            <option>quantité</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-          </select></div></div>
+            <p>article</p>
+            <div className={styles.quantite}>
+              <select>
+                <option>quantité</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select></div></div>
           <div className={styles.box}>
-            <p>Commande :</p>
+            <div className ={styles.title2}> <p>Commande :</p></div>
             <div className={styles.box2}>
               <p>quantité produit</p>
               <div className={styles.trait_dessous2}></div>
@@ -29,12 +29,12 @@ export class panier extends Component {
               <div className={styles.trait_dessous2}></div>
               <p>prix livraison :</p>
               <div className={styles.trait_dessous2}></div>
-              <p>total commande : 687 $</p>
-              </div>
+              <p>Total : 687 $</p>
+            </div>
           </div>
           <div className={styles.trait_dessous}></div>
-        </div>  
-          <div className={styles.Footer}><Footer/></div>
+        </div>
+        <div className={styles.Footer}><Footer /></div>
       </div>
     )
   }
